2862953278a8c812a94debdf53f01f040bee847d
snnxwww <kaydence.cheung@icloud.com>
2025-11-01 23:17:56 -0600

    Create baseData.js

---
+++ baseData.js
@@ -0,0 +1,31 @@
+const colour = {
+	black: [],
+	cream: [],
+	golden: [],
+	red: [],
+}
+
+const gradient = {
+	solid: [],
+	countershade: [],
+}
+
+const shade = {
+	dark: [],
+	medium: [],
+	light: [],
+}
+
+const rarity = {
+	common: [],
+	uncommon: [],
+	rare: [],
+	special: [],
+}
+
+const class = {
+	basechanger: [],
+	applicator: [],
+	breedonly: [],
+	combo: [],
+}
