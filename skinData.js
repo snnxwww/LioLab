cdd419b01ad6889fe377fdb4693f51b46581ff95
snnxwww <kaydence.cheung@icloud.com>
2025-11-02 19:44:27 -0700

    Update skinData.js

--- skinData.js
+++ skinData.js
@@ -1,5 +1,8 @@
 const type = {
-	skinchanger: [],
-	applicator: [],
-	breedonly: [],
+	skinchanger: ["apricot", "black", "blue", "brown", "burnt", "butterfly", "cinder", "coral", "dark", "darkbrown", "dudley", 
+				  "dusky", "fallow", "gray", "marbled", "matte", "orange", "pink", "plain", "quartz", "red", "sandy", "sanguine", 
+				  "tan", "tawny", "white", "yellow"],
+	applicator: ["bat", "doubloon", "growth", "lagoon", "lavender"],
+	breedonly: ["badawi", "bunting", "clouded", "echo", "eggshellblue", "greige", "ink", "lilac", "pale", "peach", "plum", "quail", 
+				"ruffian", "sanstone", "welsummer"],
 }
